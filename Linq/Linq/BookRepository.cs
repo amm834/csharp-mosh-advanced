4fdf4b9686c28507f234f810cde40890e6d17aa4
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-04 04:13:52 +0630

    Linq done

---
+++ Linq/Linq/BookRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Linq;
+
+class BookRepository
+{
+    public IEnumerable<Book> GetBooks() => new List<Book>()
+    {
+        new Book() {Title = "PHP", Price = 20},
+        new Book() {Title = "JS", Price = 30},
+        new Book() {Title = "C#", Price = 10},
+        new Book() {Title = "Java", Price = 2},
+    };
+}

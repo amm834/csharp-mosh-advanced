ab0cfdbfa17ef50281dc66ead79bd0213d538c23
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-04 03:05:14 +0630

    Advanced usage of Lambda
    It is just basic in js and php

---
+++ Lambda/Lambda/Book.cs
@@ -0,0 +1,7 @@
+﻿namespace Lambda;
+
+public class Book
+{
+    public string Title { get; set; }
+    public int Price { get; set; }
+}
---
+++ Lambda/Lambda/BookRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Lambda;
+
+public class BookRepository
+{
+    public List<Book> GetBooks() => new List<Book>()
+    {
+        new Book() {Title = "I don't know", Price = 300},
+        new Book() {Title = "I don't know 2", Price = 400},
+    };
+}

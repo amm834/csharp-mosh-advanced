13d88c2ee4b83a957a7eed2980d597098cdbf0b2
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-05-04 06:57:18 +0630

    Using nullable type and null collapsing

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var books = new BookRepository().GetBooks();
+
+        // query version
+        // var books = from book in books where book.Price < 10;
+
+        var cheapBooks = books
+            .Where(book => book.Price > 5)
+            .OrderBy(book => book.Title)
+            .Select(book => book.Title);
+
+        foreach (var cheapBook in cheapBooks)
+        {
+            Console.WriteLine(cheapBook);
+        }
+
+        var pagedBooks = books
+            .Skip(2)
+            .Take(3);
+        var totalPrice = books.Sum(book => book.Price);
+        Console.WriteLine(totalPrice);
+    }
+}
---
+++ NullableTypes/NullableTypes/Program.cs
@@ -0,0 +1,7 @@
+﻿DateTime? date = null;
+DateTime date2 = date ?? date.GetValueOrDefault();
+
+Console.WriteLine(date.GetValueOrDefault());
+Console.WriteLine(date.HasValue);
+
+Console.WriteLine(date2);
